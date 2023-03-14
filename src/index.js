001ab074c3e9a0e51fdd1aca4adb3057896a3717
nguyet-phan <nguyetpt237@gmail.com>
2023-03-14 11:11:42 +0700

    create routes, app/controllers

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({
@@ -22,27 +24,9 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+// Routes init
+route(app);
 
-app.get('/', (req, res) => {
-	res.render('home');
-})
-
-app.get('/news', (req, res) => {
-	res.render('news');
-})
-
-app.get('/search', (req, res) => {
-	///serach?q=nguyet phan & ref=mycv
-	// console.log(req.query.q);
-	res.render('search');
-})
-
-app.post('/search', (req, res) => {
-	console.log(req.body);//ket hop voi code dong 10, 11
-	res.send('');
-})
-
-//localhost - 127.0.0.1
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
