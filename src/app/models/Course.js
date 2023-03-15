95ce47679f5be0be50915fe0f4a968fc88d57713
nguyet-phan <nguyetpt237@gmail.com>
2023-03-15 17:01:17 +0700

    create new course page

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    name: { type: String, maxLength: 255, },
+    name: { type: String, required: true, },
     description: { type: String, maxLength: 600, },
-    image: { type: String, maxLength: 255, },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+    image: { type: String, },
+    videoId: { type: String, required: true, },
+    level: { type: String, },
+    slug: { type: String, slug: 'name' },
+    // slug: { type: String, slug: 'name', unique: true }, //các slug là duy nhất(unique), không được trùng nhau
+}, {
+    timestamps: true,
 });
 
 module.exports = mongoose.model('Course', Course);
