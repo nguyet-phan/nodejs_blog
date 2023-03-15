b024a889870f005d592ae0c2f7262155620123a4
nguyet-phan <nguyetpt237@gmail.com>
2023-03-15 10:13:21 +0700

    Read from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,12 +1,16 @@
-
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
     index(req, res, next) {
-
+        //từ phiên bản mongoose 6. ko hỗ trợ callback => sử dụng model với promise
         Course.find({})
-            .then(courses => res.render('home'))
+            .then(courses => {
+                res.render('home', {
+                    courses: multipleMongooseToObject(courses)
+                });
+            })
             .catch(next);
     }
 
